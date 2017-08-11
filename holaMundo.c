1066e7b985346a8915b2dcbeae369981dab4ae92
Monserrat Genereux <monserratgenereux@gmail.com>
2017-08-10 19:46:43 -0500

    update hello world with for

--- holaMundo.c
+++ holaMundo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
-	printf("Hello World\n");
+int main(int argn, char **argv){
+	//int argn numero de parametros +1 (el nombre)
+	// char ** argv; dirección de una dirección
+	//char nombre [20] puedes guardar 19 letras, se usa en lugar de string
+	//Pointer *, char * p
+	//p=&a;  guardando en p la direccion de a
+	//*p= 'B', se guarda el contenido
+	
+	for (int i = 0; i < argn; ++i)
+	{
+		printf("Hello World %s \n", argv[i]);
+	}
 	return 0;
 }
