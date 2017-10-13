098b30d566b80fd165127fd1201a4fed4b42ba94
Monserrat Genereux <monserratgenereux@gmail.com>
2017-10-12 19:37:58 -0500

    add bsearch

--- qsort/main.c
+++ qsort/main.c
@@ -2,28 +2,40 @@
 #include <stdio.h>
 #include "qsort.h"
 #include <string.h>
+#define MAX_LENGTH_ORDER 20
+#define STUDENTS_LENGTH 3
 
 typedef struct s{
 	char *name;
 	int matricula;
 }Student;
 
-int cmpNames(void const *element1, void const *element2){
+int cmpStudents(void const *element1, void const *element2, int attribute){
 	Student *student1=(Student *)element1;
 	Student *student2=(Student *)element2;
-	return strcmp(student1->name,student2->name);
+	switch(attribute){
+		case 0:
+			return strcmp(student1->name,student2->name);
+		case 1:
+			return student1->matricula - student2->matricula;
+	}
+}
+
+int cmpNames(void const *element1, void const *element2){
+	return cmpStudents(element1,element2,0);
 }
 
 int cmpMatriculas(void const *element1, void const *element2){
-	Student *student1=(Student *)element1;
-	Student *student2=(Student *)element2;
-	return student1->matricula - student2->matricula;
+	return cmpStudents(element1,element2,1);
 }
 
 int main(int argc, char **argv) {
 
-  char order[2];		
-  Student group[3];
+  char order[MAX_LENGTH_ORDER];	
+  int neededStudent;
+  Student key;
+  Student *foundStudent;	
+  Student group[STUDENTS_LENGTH];
   group[0].name = strdup("Juanito");
   group[0].matricula=17;
 
@@ -36,14 +48,25 @@ int main(int argc, char **argv) {
   scanf("%s",order);
 
   if(!strcmp(order,"name")){
-  	qsort(group,3,sizeof(Student),cmpNames);
+  	qsort(group,STUDENTS_LENGTH,sizeof(Student),cmpNames);
   }if(!strcmp(order,"matricula")){
-  	qsort(group,3,sizeof(Student),cmpMatriculas);
+  	qsort(group,STUDENTS_LENGTH,sizeof(Student),cmpMatriculas);
   }
 
   for (int i = 0; i < 3; ++i)
   {
   	printf("Student %s %d\n", group[i].name,group[i].matricula);
   }
+  printf("Which is the id: ");
+  scanf("%d",&neededStudent);
+
+  key.matricula=neededStudent;
+  foundStudent=bsearch(&key,group,STUDENTS_LENGTH,sizeof(Student),cmpMatriculas);
+  if(foundStudent!=NULL){
+  	printf("Student %s %d\n", foundStudent->name,foundStudent->matricula);
+  }else{
+  	printf("Student not found with id %d\n",neededStudent);
+  }
+
   return 0;
 }
