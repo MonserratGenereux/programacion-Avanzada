7c62ff7716121370e2b02c99da2ee6d7ad509424
Monserrat Genereux <monserratgenereux@gmail.com>
2017-11-02 19:43:37 -0600

    add 100 aproach

--- thread/main.c
+++ thread/main.c
@@ -5,11 +5,13 @@
 #include <stdio.h>
 #define NUM_THREADS 5
 
- void *PrintHello(void *threadid)
+unsigned int total;
+ void *add100(void *threadid)
  {
     long tid;
     tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
+    printf("Adding 100 #%ld!\n", tid);
+    total +=100;
     pthread_exit(NULL);
  }
 
@@ -18,14 +20,19 @@
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+    total =0;
+    printf("Initial total =%d\n", total);
     for(t=0; t<NUM_THREADS; t++){
-       printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       printf("Final total: %d\n", total);
+       rc = pthread_create(&threads[t], NULL, add100, (void *)t);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
           exit(-1);
        }
     }
+    for(t=0; t<NUM_THREADS; t++){
+       pthread_join (threads[t], NULL);
+       }
 
     /* Last thing that main() should do */
     pthread_exit(NULL);
