235524c7a1f9a5a28056c39e663f9d1425bde4db
Monserrat Genereux <monserratgenereux@gmail.com>
2017-11-09 15:41:40 -0600

    update

--- thread/main.c
+++ thread/main.c
@@ -1,44 +1,56 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "thread.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#define NUM_THREADS 5
+#include <semaphore.h>
+#define NUM_THREADS 10
 
-unsigned int total;
- void *add100(void *threadid)
- {
+sem_t forks[NUM_THREADS];
+
+void thinking(int n) {
+    sleep(rand()%n);
+}
+
+void eating(int n) {
+    sleep(rand()%n);
+}
+
+void *lifestyle(void *threadid) {
     long tid;
-    tid = (long)threadid;
-    unsigned int localTotal;
-    printf("Adding 100 #%ld!\n", tid);
-    localTotal= total;
-    localTotal +=100;
-    sleep(tid%5);
-    total = localTotal;
-    pthread_exit(NULL);
- }
+    tid = (long) threadid;
+    while(1) {
+        thinking(3);
+        printf("I am philosopher %lu, I am hungry\n", tid);
+        sem_wait(&forks[tid]);
+        sem_wait(&forks[(tid + 1) % NUM_THREADS]);
+        eating(4);
+        sem_post(&forks[tid]);
+        sem_post(&forks[(tid + 1) % NUM_THREADS]);
+        printf("I am philosopher %lu, I am thinking\n", tid);
+    }
+}
 
- int main (int argc, char *argv[])
- {
+int main(int argc, char ** argv) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
-    total =0;
-    printf("Initial total =%d\n", total);
-    for(t=0; t<NUM_THREADS; t++){
-       printf("Final total: %d\n", total);
-       rc = pthread_create(&threads[t], NULL, add100, (void *)t);
-       if (rc){
-          printf("ERROR; return code from pthread_create() is %d\n", rc);
-          exit(-1);
-       }
-    }
-    for(t=0; t<NUM_THREADS; t++){
-       pthread_join (threads[t], NULL);
-       }
 
-    /* Last thing that main() should do */
+    for (t = 0; t < NUM_THREADS; t++) {
+        sem_init(&forks[t], 0, 1);
+    }
+
+    for (t=0; t < NUM_THREADS; t++) {
+        printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, lifestyle, (void *)t);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
     pthread_exit(NULL);
- }
+}
